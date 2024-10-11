9c61187fb070e85e1fd8ee230d2841e912e8b8c3
nithyaganesh4343@gmail.com <nithyaganesh.dev@gmail.com>
2024-10-11 13:37:26 +0530

    d

--- divTinder/src/app.js
+++ divTinder/src/app.js
@@ -4,13 +4,18 @@ const app = express();
 const {auth}=require('./middlewares/auth');
 
 app.use("/user/sayHi",auth,(req,res)=>{
-    throw new Error("He he he");
     res.send("Hi");
 });
 app.use("/",(err,req,res,next)=>{
     if(err){
         res.status(500).send("Iyoo error uh");
     }
+
+})
+
+
+app.use("/",(req,res)=>{
+    res.send("Server is running ");
 })
 app.listen(3000,()=>{
     console.log("server running on http://localhost:3000 successfully");
